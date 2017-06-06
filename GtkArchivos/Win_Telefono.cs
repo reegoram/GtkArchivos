c6a5f81f2729d3b0ec21e0b13d52ab018c045187
Rigo Ramirez <ram_ri@outlook.com>
2017-06-06 11:45:00 -0500

    Image saved on file .dat

--- GtkArchivos/Win_Telefono.cs
+++ GtkArchivos/Win_Telefono.cs
@@ -8,108 +8,89 @@ namespace GtkArchivos
 {
 	public partial class Win_Telefono : Gtk.Window
 	{
-		
-		// string ruta = @"C:\TELEFONO\telefono.dat";	/* Ruta Windows */
-		string ruta = @"telefono.dat";	/* Ruta Linux(root) */
-
 		op_Telefono Telefono;
 		Gtk.ListStore DataTel;
-		
-		int id = 0; 
-		string nombre = string.Empty;
-		string marca = string.Empty;
-		string modelo = string.Empty;
-		string compania = string.Empty;
 
-		public Win_Telefono () :
-			base (Gtk.WindowType.Toplevel)
+		public Win_Telefono() :
+			base(Gtk.WindowType.Toplevel)
 		{
-			this.Build ();
+			this.Build();
 
-			Telefono = new op_Telefono (ruta);
+			Telefono = new op_Telefono();
 			//Telefono.LeerDatos (ruta, entryID);
-			DataTel = Telefono.GenerarTreeView (tvVerDatos, DataTel);
+			DataTel = Telefono.GenerarTreeView(tvVerDatos, DataTel);
 
 		}
 
-		protected void OnBtnGuardarClicked (object sender, EventArgs e)
-		{			
-			try {				
-				id = int.Parse (entryID.Text);
-				nombre = entryNombre.Text;
-				marca = entryMarca.Text;
-				modelo = entryModelo.Text;
-				compania = entryCompania.Text;
+		protected void OnBtnGuardarClicked(object sender, EventArgs e)
+		{
+			try
+			{
 
-				
-				string d = Telefono.InsertarDatos (ruta, id, nombre, marca, modelo, compania);
+				Telefono.id = int.Parse(entryID.Text);
+				Telefono.nombre = entryNombre.Text;
+				Telefono.marca = entryMarca.Text;
+				Telefono.modelo = entryModelo.Text;
+				Telefono.compania = entryCompania.Text;
+
+				string d = Telefono.InsertarDatos();
 				if (d == "Guardado")
-					MessageBox.Show ("Datos Guardados Exitosamente", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					MessageBox.Show("Datos Guardados Exitosamente", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
 				else
-					MessageBox.Show ("Error de Guardado", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					MessageBox.Show("Error de Guardado", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
-				string y = Telefono.LeerDatos(ruta);
-				if (y == "Leido") {					
+				/*string y = Telefono.LeerDatos();
+				if (y == "Leido")
+				{
 					//DataTel.AppendValues(br.ReadInt32(), br.ReadString(), br.ReadString(), br.ReadString(), br.ReadString());
-					
+
 					/* Agregando Datos al Modelo */
-					DataTel.AppendValues (id.ToString (), nombre.ToString (), marca.ToString (), modelo.ToString (), compania.ToString ()); 
-					
-				}
-				else{
+				//DataTel.AppendValues(id.ToString(), nombre.ToString(), marca.ToString(), modelo.ToString(), compania.ToString());
+
+				/*}
+				else
+				{
 					MessageBox.Show("No Leido");
 				}
 
-
+				*/
 				Telefono.ResetEntry(entryID, entryNombre, entryMarca, entryModelo, entryCompania);
-				
-			} catch (Exception ex) {
-				Telefono.ValidarEntry (entryID, entryNombre, entryMarca, entryModelo, entryCompania);
-				System.Diagnostics.Debug.WriteLine (ex.ToString());
+
+			}
+			catch (Exception ex)
+			{
+				Telefono.ValidarEntry(entryID, entryNombre, entryMarca, entryModelo, entryCompania);
+				System.Diagnostics.Debug.WriteLine(ex.ToString());
 			}
 
 
 		}
 
-		protected void OnBtnActualizarClicked (object sender, EventArgs e)
+		protected void OnBtnActualizarClicked(object sender, EventArgs e)
 		{
-			
+
 		}
 
-		protected void OnBtnEliminarClicked (object sender, EventArgs e)
+		protected void OnBtnEliminarClicked(object sender, EventArgs e)
 		{
-			
+
 		}
 
-		protected void OnBtnSalirClicked (object sender, EventArgs e)
+		protected void OnBtnSalirClicked(object sender, EventArgs e)
 		{
-			Gtk.Application.Quit ();
+			Gtk.Application.Quit();
 		}
 
-		protected void OnBtnSelecImagenClicked (object sender, EventArgs e)
+		protected void OnBtnSelecImagenClicked(object sender, EventArgs e)
 		{
-			Telefono.SeleccionarImagen (imgVisual, this);
+			Telefono.SeleccionarImagen(imgVisual, this);
 		}
-
-
 
 		/*** FUNCIONES ***/
+		public void EliminarDatosTreeView(Gtk.TreeView treeV)
+		{
 
-
-		public void EliminarDatosTreeView(Gtk.TreeView treeV){
-			
 		}
-
-
-
-
-
-
-
-
-
-
-		
 	}
 }
 
